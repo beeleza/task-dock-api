5df301d26c1beb5458cb4151d6578b01a92940d3
beeleza <higorbeleza.dev@gmail.com>
2025-11-14 13:32:43 -0300

    refactor: organizing routes and startup file

--- src/app.js
+++ src/app.js
@@ -3,27 +3,23 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 const cookieParser = require("cookie-parser");
-
-const corsOptions = {
-  origin: [process.env.FRONTEND_URL],
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-  credentials: true,
-};
+const routes = require("./routes");
 
 const app = express();
-const PORT = process.env.PORT || 4000;
 
 app.use(helmet());
-app.use(cors());
+
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use(cookieParser());
 
-app.get("", (req, res) => {
-  res.send("Hello world");
-});
+app.use("/api/v1", routes);
 
-app.use("/api/v1/auth", require("./routes/authRoutes"));
-
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+module.exports = app;
